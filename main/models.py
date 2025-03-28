31f2cea83b7d1f6fabb059031de7080dd27342a1
Nazar Rachkivskiy <rachkovskiy.ua@gmail.com>
2025-03-28 16:35:31 +0200

    add shop product list, pagination

--- main/models.py
+++ main/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('main:product_list_by_category', args=[self.slug])
+
     class Meta:
         ordering = ['name']
         indexes = [models.Index(fields=['name'])]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
 
 
 def popular_list(request):
@@ -16,3 +17,21 @@ def product_detail(request, slug):
                   {'product': product})
 
 
+def product_list(request, category_slug=None):
+    page = request.GET.get('page', 1)
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    paginator = Paginator(products, 10)
+    current_page = paginator.page(int(page))
+    if category_slug:
+        category = get_object_or_404(Category,
+                                     slug=category_slug)
+        paginator = Paginator(products.filter(category=category), 10)
+        current_page = paginator.page(int(page))
+    return render(request,
+                  'main/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': current_page,
+                   'slug_url': category_slug})
